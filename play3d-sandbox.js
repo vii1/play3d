184fcd8a514eb4148698645f6ef3ffe16e2e205c
vii <vii@divero.net>
2018-11-01 22:20:15 +0100

    Handle resizing

--- play3d-sandbox.js
+++ play3d-sandbox.js
@@ -53,6 +53,30 @@ var RT = (function() {
     clear3d();
     create3d();
 
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+
+    window.addEventListener('optimizedResize', function(e) {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        render();
+    });
+
     var obj = {
         clear3d: clear3d,
         render: render,
